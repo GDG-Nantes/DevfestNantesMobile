1f8d140609cb5a68da5667334af9189157ee0cd4
Robin Caroff <robin.caroff@gmail.com>
2022-07-15 23:15:21 +0200

    📣 Adds an App Initializer system.

---
+++ androidApp/src/main/java/com/gdgnantes/devfest/android/core/ApplicationInitializer.kt
@@ -0,0 +1,4 @@
+package com.gdgnantes.devfest.android.core
+
+interface ApplicationInitializer : SuspendUseCase<Unit, Unit> {
+}
--- androidApp/src/main/java/com/gdgnantes/devfest/android/core/injection/AppModule.kt
+++ androidApp/src/main/java/com/gdgnantes/devfest/android/core/injection/AppModule.kt
@@ -1,17 +1,37 @@
 package com.gdgnantes.devfest.android.core.injection
 
+import com.gdgnantes.devfest.android.core.ApplicationInitializer
+import com.gdgnantes.devfest.android.core.CoroutinesDispatcherProvider
+import com.gdgnantes.devfest.android.core.LoggerInitializer
 import com.gdgnantes.devfest.store.DevFestNantesStore
 import com.gdgnantes.devfest.store.DevFestNantesStoreMocked
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.ElementsIntoSet
+import kotlinx.coroutines.Dispatchers
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class AppModule {
 
     companion object {
+        @AppScope
+        @Provides
+        fun coroutinesDispatcherProvider() = CoroutinesDispatcherProvider(
+            default = Dispatchers.Default,
+            computation = Dispatchers.Default,
+            io = Dispatchers.IO,
+            main = Dispatchers.Main.immediate
+        )
+
+        @Provides
+        @ElementsIntoSet
+        fun applicationInitializers(
+            loggerInitializer: LoggerInitializer
+        ): Set<ApplicationInitializer> = setOf(loggerInitializer)
+
         @AppScope
         @Provides
         fun store(): DevFestNantesStore = DevFestNantesStoreMocked()
