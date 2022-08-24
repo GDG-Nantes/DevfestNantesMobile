4ca9341ebe684755bc570899c4724859acde3d44
Robin Caroff <robin.caroff@gmail.com>
2022-08-24 17:48:12 +0200

    📣 Adds rooms data.

--- shared/src/commonMain/kotlin/com/gdgnantes/devfest/model/stubs/RoomStubs.kt
+++ shared/src/commonMain/kotlin/com/gdgnantes/devfest/model/stubs/RoomStubs.kt
@@ -1,2 +1,36 @@
 package com.gdgnantes.devfest.model.stubs
 
+import com.gdgnantes.devfest.model.Room
+import kotlin.native.concurrent.SharedImmutable
+
+@SharedImmutable
+val roomStubs: List<Room> = listOf(
+    Room(
+        "jules_verne",
+        "Jules Verne"
+    ),
+    Room(
+        "titan",
+        "Titan"
+    ),
+    Room(
+        "belem",
+        "Belem"
+    ),
+    Room(
+        "tour_de_bretagne",
+        "Tour de Bretagne"
+    ),
+    Room(
+        "les_machines",
+        "Les Machines"
+    ),
+    Room(
+        "Hangar",
+        "Hangar"
+    ),
+    Room(
+        "l_atelier",
+        "L'Atelier"
+    )
+)
