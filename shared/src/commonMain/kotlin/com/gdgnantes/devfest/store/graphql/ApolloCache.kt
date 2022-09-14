05a913f2d5ece3f147e611d0f799f66cd9972d11
Robin Caroff <robin.caroff@gmail.com>
2022-09-14 14:59:29 +0200

    🐛 Fixes iOS SQLite cache not working (#24).

--- shared/src/commonMain/kotlin/com/gdgnantes/devfest/store/graphql/ApolloCache.kt
+++ shared/src/commonMain/kotlin/com/gdgnantes/devfest/store/graphql/ApolloCache.kt
@@ -1,5 +1,8 @@
 package com.gdgnantes.devfest.store.graphql
 
+import com.apollographql.apollo3.cache.normalized.api.MemoryCacheFactory
 import com.apollographql.apollo3.cache.normalized.api.NormalizedCacheFactory
+import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
 
-expect val normalizedCache: NormalizedCacheFactory
+val normalizedCache: NormalizedCacheFactory = MemoryCacheFactory(10 * 1024 * 1024)
+    .chain(SqlNormalizedCacheFactory("apollo.db"))
