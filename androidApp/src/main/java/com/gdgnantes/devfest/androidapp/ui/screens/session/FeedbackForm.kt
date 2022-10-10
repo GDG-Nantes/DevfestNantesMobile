63f507b70546a767ef59d616ea8cfb5a3c394cd3
Robin Caroff <robin.caroff@gmail.com>
2022-10-10 22:18:21 +0200

    📣 Adds remote config to enable/disable openfeedback form.

--- androidApp/src/main/java/com/gdgnantes/devfest/androidapp/ui/screens/session/FeedbackForm.kt
+++ androidApp/src/main/java/com/gdgnantes/devfest/androidapp/ui/screens/session/FeedbackForm.kt
@@ -5,10 +5,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.gdgnantes.devfest.androidapp.BuildConfig
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.gdgnantes.devfest.androidapp.R
 import io.openfeedback.android.OpenFeedback
 import io.openfeedback.android.components.SessionFeedbackContainer
@@ -17,10 +18,11 @@ import java.util.*
 @Composable
 fun FeedbackForm(
     modifier: Modifier = Modifier,
+    feedbackFormViewModel: FeedbackFormViewModel = hiltViewModel(),
     openFeedback: OpenFeedback,
     openFeedbackFormId: String
 ) {
-    if (BuildConfig.OPEN_FEEDBACK_ENABLED.toBoolean()) {
+    if (feedbackFormViewModel.isOpenFeedbackEnabled.collectAsState().value) {
         Column(
             modifier = modifier
         ) {
