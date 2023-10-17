83d8dfb8021af482c2d54a4e669398b5abedf6b5
Stéphane Rihet <stephane.rihet@gmail.com>
2023-10-17 11:47:04 +0200

    👍 Improves management of favorites.

--- iosApp/iosApp/New Group/Agenda/AgendaView.swift
+++ iosApp/iosApp/New Group/Agenda/AgendaView.swift
@@ -10,150 +10,222 @@ import SwiftUI
 import shared
 
 struct AgendaView: View {
-    //Store an observable object instance
     @ObservedObject var viewModel = AgendaViewModel()
     
-    //Property wrapper type that can read and write a value managed by SwiftUI
     @State private var day = "2023-10-19"
     @State private var showFavoritesOnly = false
     @State private var selectedRoom: Room?
     @State private var selectedComplexity: Complexity?
     @State private var selectedLanguage: SessionLanguage?
     @State private var selectedSessionType: SessionType?
-    @State private var clearFilter = false
+    @State private var scrollToSectionID: Date? = nil
+    @State private var firstAppear = true
     
-    //Section Time formatter
     var sectionTimeFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm"
         return formatter
     }()
     
-    //Setup UI
     var body: some View {
-        
         NavigationView {
             LoadingView(isShowing: $viewModel.isLoading) {
-                VStack {
-                    Picker("What is the day?", selection: $day) {
-                        Text(L10n.day1).tag("2023-10-19")
-                        Text(L10n.day2).tag("2023-10-20")
-                    }
-                    .pickerStyle(.segmented)
-                    List {
-                            ForEach(viewModel.agendaContent.sections.filter{($0.day.contains(day))}, id: \.date) { section in
-                                Section(header: Text("\(self.sectionTimeFormatter.string(from: section.date))")) {
-                                    let filteredSessions = getFilteredSessions(sessions: section.sessions)
-                                    ForEach(self.showFavoritesOnly ? filteredSessions.filter({viewModel.favorites.contains($0.id)}):  filteredSessions, id: \.id) { session in
-                                        if session.sessionType == .conference || session.sessionType == .codelab || session.sessionType == .quickie{
-                                            NavigationLink(destination: AgendaDetailView(session: session, day: day)) {
+                ScrollViewReader { proxy in
+                    VStack {
+                        Picker("What is the day?", selection: $day) {
+                            Text(L10n.day1).tag("2023-10-19")
+                            Text(L10n.day2).tag("2023-10-20")
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+
+                        List {
+                            ForEach(viewModel.agendaContent.sections.filter { $0.day.contains(day) }, id: \.date) { section in
+                                let filteredSessions = getFilteredSessions(sessions: section.sessions)
+                                let sessionsToShow = showFavoritesOnly ? filteredSessions.filter { viewModel.favorites.contains($0.id) } : filteredSessions
+                                
+                                if !sessionsToShow.isEmpty {
+                                    Section(header: Text(sectionTimeFormatter.string(from: section.date)).id(section.date)) {
+                                        ForEach(sessionsToShow, id: \.id) { session in
+                                            if session.sessionType == .conference || session.sessionType == .codelab || session.sessionType == .quickie {
+                                                NavigationLink(destination: AgendaDetailView(session: session, day: day)) {
+                                                    AgendaCellView(session: session)
+                                                }
+                                            } else {
                                                 AgendaCellView(session: session)
                                             }
-                                        } else {
-                                            AgendaCellView(session: session)
                                         }
                                     }
                                 }
                             }
-                    }
-                    .navigationBarTitle(L10n.screenAgenda)
-                    .navigationBarItems(trailing:
-                                            Menu("\(Image(systemName: "line.3.horizontal.decrease.circle"))") {
-                        let selected = Binding(
-                            get: { self.showFavoritesOnly },
-                            set: { self.showFavoritesOnly = $0 == self.showFavoritesOnly ? false : $0 }
-                        )
-                        Picker("", selection: selected) {
-                            Text(L10n.filterFavorites).tag(true)
-                            Menu(L10n.sessionFiltersDrawerLanguagesLabel) {
-                                let selected = Binding(
-                                    get: { self.selectedLanguage },
-                                    set: { self.selectedLanguage = $0 == self.selectedLanguage ? nil : $0 })
-                                Picker(L10n.sessionFiltersDrawerLanguagesLabel, selection: selected) {
-                                    Text(L10n.languageFrench).tag(Optional(SessionLanguage.french))
-                                    Text(L10n.languageEnglish).tag(Optional(SessionLanguage.english))
-                                }
-                            }
-                            Menu(L10n.sessionFiltersDrawerComplexityLabel) {
-                                let selected = Binding(
-                                    get: { self.selectedComplexity },
-                                    set: { self.selectedComplexity = $0 == self.selectedComplexity ? nil : $0 })
-                                Picker(L10n.sessionFiltersDrawerComplexityLabel, selection: selected) {
-                                    Text(L10n.complexityBeginer).tag(Optional(Complexity.beginner))
-                                    Text(L10n.complexityIntermediate).tag(Optional(Complexity.intermediate))
-                                    Text(L10n.complexityAdvanced).tag(Optional(Complexity.advanced))
-                                }
-                            }
-                            if let rooms = viewModel.roomsContent {
-                                Menu(L10n.sessionFiltersDrawerRoomsLabel) {
-                                    let selected = Binding(
-                                        get: { self.selectedRoom },
-                                        set: { self.selectedRoom = $0 == self.selectedRoom ? nil : $0 })
-                                    Picker(L10n.sessionFiltersDrawerRoomsLabel, selection: selected) {
-                                        ForEach(rooms, id: \.id) { room in
-                                            Text(room.name).tag(Optional(room))
-                                        }
-                                    }
-                                }}
-                            Menu(L10n.sessionFiltersDrawerTypeLabel) {
-                                let selected = Binding(
-                                    get: { self.selectedSessionType },
-                                    set: { self.selectedSessionType = $0 == self.selectedSessionType ? nil : $0 })
-                                Picker(L10n.sessionFiltersDrawerTypeLabel, selection: selected) {
-                                    Text(L10n.sessionTypeConference).tag(Optional(SessionType.conference))
-                                    Text(L10n.sessionTypeQuickie).tag(Optional(SessionType.quickie))
-                                    Text(L10n.sessionTypeCodelab).tag(Optional(SessionType.codelab))
-                                }
+                        }
+                        .onChange(of: scrollToSectionID) { newID in
+                            if let newID = newID {
+                                proxy.scrollTo(newID, anchor: .top)
                             }
                         }
-                        ///Adds remove button filters
-                        if showFavoritesOnly || selectedRoom != nil || selectedLanguage != nil || selectedComplexity != nil || selectedSessionType != nil {
-                            Button(action: {
-                                self.showFavoritesOnly = false
-                                self.selectedRoom = nil
-                                self.selectedLanguage = nil
-                                self.selectedSessionType = nil
-                                self.selectedComplexity = nil
-                            }) {
-                                Label(L10n.filterClear, systemImage: "trash")
-                            }
-                        }
-                        
-                    })
-                    .task {
-                        RCValues.sharedInstance.fetchCloudValues()
-                        await viewModel.observeRooms()
-                        await viewModel.observeSessions()
                     }
                 }
             }
+            .onChange(of: viewModel.isLoading) { isLoading in
+                if !isLoading {
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "yyyy-MM-dd"
+                    let currentDay = formatter.string(from: Date())
+                    
+                    if currentDay == day && firstAppear {
+                        let calendar = Calendar.current
+                        let now = Date()
+                        let nowHour = calendar.component(.hour, from: now)
+                        let nowMinute = calendar.component(.minute, from: now)
+                        let nowTotalMinutes = nowHour * 60 + nowMinute
+                        
+                        let sortedSections = viewModel.agendaContent.sections
+                            .filter({ $0.day.contains(day) })
+                            .sorted(by: { section1, section2 in
+                                let hour1 = calendar.component(.hour, from: section1.date)
+                                let minute1 = calendar.component(.minute, from: section1.date)
+                                let totalMinutes1 = hour1 * 60 + minute1
+                                
+                                let hour2 = calendar.component(.hour, from: section2.date)
+                                let minute2 = calendar.component(.minute, from: section2.date)
+                                let totalMinutes2 = hour2 * 60 + minute2
+                                
+                                return totalMinutes1 < totalMinutes2
+                            })
+                        
+                        var previousSection: AgendaContent.Section?
+                        
+                        for section in sortedSections {
+                            let hour = calendar.component(.hour, from: section.date)
+                            let minute = calendar.component(.minute, from: section.date)
+                            let sectionTotalMinutes = hour * 60 + minute
+                            
+                            if sectionTotalMinutes <= nowTotalMinutes {
+                                previousSection = section
+                            } else {
+                                break
+                            }
+                        }
+                        
+                        if let previousSection = previousSection {
+                            DispatchQueue.main.async {
+                                scrollToSectionID = previousSection.date
+                            }
+                        }
+                        
+                        firstAppear = false
+                    }
+                }
+            }
+
+            .navigationBarTitle(L10n.screenAgenda)
+            .navigationBarItems(trailing:
+                                    Menu("\(Image(systemName: "line.3.horizontal.decrease.circle"))") {
+                let selected = Binding(
+                    get: { self.showFavoritesOnly },
+                    set: { self.showFavoritesOnly = $0 == self.showFavoritesOnly ? false : $0 }
+                )
+                Picker("", selection: selected) {
+                    Text(L10n.filterFavorites).tag(true)
+                    Menu(L10n.sessionFiltersDrawerLanguagesLabel) {
+                        let selected = Binding(
+                            get: { self.selectedLanguage },
+                            set: { self.selectedLanguage = $0 == self.selectedLanguage ? nil : $0 })
+                        Picker(L10n.sessionFiltersDrawerLanguagesLabel, selection: selected) {
+                            Text(L10n.languageFrench).tag(Optional(SessionLanguage.french))
+                            Text(L10n.languageEnglish).tag(Optional(SessionLanguage.english))
+                        }
+                    }
+                    Menu(L10n.sessionFiltersDrawerComplexityLabel) {
+                        let selected = Binding(
+                            get: { self.selectedComplexity },
+                            set: { self.selectedComplexity = $0 == self.selectedComplexity ? nil : $0 })
+                        Picker(L10n.sessionFiltersDrawerComplexityLabel, selection: selected) {
+                            Text(L10n.complexityBeginer).tag(Optional(Complexity.beginner))
+                            Text(L10n.complexityIntermediate).tag(Optional(Complexity.intermediate))
+                            Text(L10n.complexityAdvanced).tag(Optional(Complexity.advanced))
+                        }
+                    }
+                    if let rooms = viewModel.roomsContent {
+                        Menu(L10n.sessionFiltersDrawerRoomsLabel) {
+                            let selected = Binding(
+                                get: { self.selectedRoom },
+                                set: { self.selectedRoom = $0 == self.selectedRoom ? nil : $0 })
+                            Picker(L10n.sessionFiltersDrawerRoomsLabel, selection: selected) {
+                                ForEach(rooms, id: \.id) { room in
+                                    Text(room.name).tag(Optional(room))
+                                }
+                            }
+                        }}
+                    Menu(L10n.sessionFiltersDrawerTypeLabel) {
+                        let selected = Binding(
+                            get: { self.selectedSessionType },
+                            set: { self.selectedSessionType = $0 == self.selectedSessionType ? nil : $0 })
+                        Picker(L10n.sessionFiltersDrawerTypeLabel, selection: selected) {
+                            Text(L10n.sessionTypeConference).tag(Optional(SessionType.conference))
+                            Text(L10n.sessionTypeQuickie).tag(Optional(SessionType.quickie))
+                            Text(L10n.sessionTypeCodelab).tag(Optional(SessionType.codelab))
+                        }
+                    }
+                }
+                ///Adds remove button filters
+                if showFavoritesOnly || selectedRoom != nil || selectedLanguage != nil || selectedComplexity != nil || selectedSessionType != nil {
+                    Button(action: {
+                        self.showFavoritesOnly = false
+                        self.selectedRoom = nil
+                        self.selectedLanguage = nil
+                        self.selectedSessionType = nil
+                        self.selectedComplexity = nil
+                    }) {
+                        Label(L10n.filterClear, systemImage: "trash")
+                    }
+                }
+                
+            })
+            .task {
+                RCValues.sharedInstance.fetchCloudValues()
+                await viewModel.observeRooms()
+                await viewModel.observeSessions()
+            }
         }
-        .onAppear{
+        
+        .onAppear {
+            
+            if scrollToSectionID == nil {
+                scrollToSectionID = viewModel.agendaContent.sections.first?.date
+            }
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            let currentDay = formatter.string(from: Date())
+
+            
+            if currentDay == "2023-10-19" {
+                day = "2023-10-19"
+            } else if currentDay == "2023-10-20" {
+                day = "2023-10-20"
+            } else {
+                day = "2023-10-19"
+            }
+            
+            
             FirebaseAnalyticsService.shared.pageEvent(page: AnalyticsPage.agenda, className: "AgendaView")
         }
     }
     
-    ///Method to filter sessions
-    private func getFilteredSessions(sessions: [AgendaContent.Session]) -> [AgendaContent.Session]{
+    private func getFilteredSessions(sessions: [AgendaContent.Session]) -> [AgendaContent.Session] {
         var selectedSession: [AgendaContent.Session] = sessions
         if let unwrappedLanguage = selectedLanguage {
-            selectedSession = selectedSession.filter {
-                $0.language == unwrappedLanguage
-            }
+            selectedSession = selectedSession.filter { $0.language == unwrappedLanguage }
         }
-        if let unwrappedSessiontype = selectedSessionType {
-            selectedSession = selectedSession.filter {
-                $0.sessionType == unwrappedSessiontype
-            }
+        if let unwrappedSessionType = selectedSessionType {
+            selectedSession = selectedSession.filter { $0.sessionType == unwrappedSessionType }
         }
-        if let unwrappedRooms = selectedRoom {
-            selectedSession = selectedSession.filter({unwrappedRooms.name.contains($0.room)})
+        if let unwrappedRoom = selectedRoom {
+            selectedSession = selectedSession.filter { unwrappedRoom.name.contains($0.room) }
         }
-        
         if let unwrappedComplexity = selectedComplexity {
-            selectedSession = selectedSession.filter {
-                $0.complexity == unwrappedComplexity
-            }
+            selectedSession = selectedSession.filter { $0.complexity == unwrappedComplexity }
         }
         return selectedSession
     }
--- iosApp/iosApp/UI/Speakers/SpeakerDetailsView.swift
+++ iosApp/iosApp/UI/Speakers/SpeakerDetailsView.swift
@@ -73,7 +73,6 @@ struct SpeakerDetails: View {
     }
     
     func onSocialLinkClick(socialitem: SocialItem) {
-        // Votre logique ici
     }
 }
 
