d3676545988f650c6350556e65c94ff3b7dac8d6
Robin Caroff <robin.caroff@gmail.com>
2025-07-29 00:00:08 +0200

    feat(agenda): add AgendaCellView, AgendaDetailView, AgendaView, and AgendaViewModel for session management

---
+++ iosApp/iosApp/Agenda/AgendaView.swift
@@ -0,0 +1,232 @@
+//
+//  AgendaView.swift
+//  iosApp
+//
+//  Created by Stéphane Rihet on 10/07/2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct AgendaView: View {
+    @ObservedObject var viewModel = AgendaViewModel()
+    
+    @State private var day = "2024-10-17"
+    @State private var showFavoritesOnly = false
+    @State private var selectedRoom: Room_?
+    @State private var selectedComplexity: Complexity?
+    @State private var selectedLanguage: SessionLanguage?
+    @State private var selectedSessionType: SessionType?
+    @State private var scrollToSectionID: Date? = nil
+    @State private var firstAppear = true
+    
+    var sectionTimeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter
+    }()
+    
+    var body: some View {
+        NavigationView {
+            LoadingView(isShowing: $viewModel.isLoading) {
+                ScrollViewReader { proxy in
+                    VStack {
+                        Picker("What is the day?", selection: $day) {
+                            Text(L10n.day1).tag("2024-10-17")
+                            Text(L10n.day2).tag("2024-10-18")
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+
+                        List {
+                            ForEach(viewModel.agendaContent.sections.filter { $0.day.contains(day) }, id: \.date) { section in
+                                let filteredSessions = getFilteredSessions(sessions: section.sessions)
+                                let sessionsToShow = showFavoritesOnly ? filteredSessions.filter { viewModel.favorites.contains($0.id) } : filteredSessions
+                                
+                                if !sessionsToShow.isEmpty {
+                                    Section(header: Text(sectionTimeFormatter.string(from: section.date)).id(section.date)) {
+                                        ForEach(sessionsToShow, id: \.id) { session in
+                                            if session.sessionType == .conference || session.sessionType == .codelab || session.sessionType == .quickie {
+                                                NavigationLink(destination: AgendaDetailView(session: session, day: day)) {
+                                                    AgendaCellView(session: session)
+                                                }
+                                            } else {
+                                                AgendaCellView(session: session)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        .onChange(of: scrollToSectionID) { newID in
+                            if let newID = newID {
+                                proxy.scrollTo(newID, anchor: .top)
+                            }
+                        }
+                    }
+                }
+            }
+            .onChange(of: viewModel.isLoading) { isLoading in
+                if !isLoading {
+                    let formatter = DateFormatter()
+                    formatter.dateFormat = "yyyy-MM-dd"
+                    let currentDay = formatter.string(from: Date())
+                    
+                    if currentDay == day && firstAppear {
+                        let calendar = Calendar.current
+                        let now = Date()
+                        let nowHour = calendar.component(.hour, from: now)
+                        let nowMinute = calendar.component(.minute, from: now)
+                        let nowTotalMinutes = nowHour * 60 + nowMinute
+                        
+                        let sortedSections = viewModel.agendaContent.sections
+                            .filter({ $0.day.contains(day) })
+                            .sorted(by: { section1, section2 in
+                                let hour1 = calendar.component(.hour, from: section1.date)
+                                let minute1 = calendar.component(.minute, from: section1.date)
+                                let totalMinutes1 = hour1 * 60 + minute1
+                                
+                                let hour2 = calendar.component(.hour, from: section2.date)
+                                let minute2 = calendar.component(.minute, from: section2.date)
+                                let totalMinutes2 = hour2 * 60 + minute2
+                                
+                                return totalMinutes1 < totalMinutes2
+                            })
+                        
+                        var previousSection: AgendaContent.Section?
+                        
+                        for section in sortedSections {
+                            let hour = calendar.component(.hour, from: section.date)
+                            let minute = calendar.component(.minute, from: section.date)
+                            let sectionTotalMinutes = hour * 60 + minute
+                            
+                            if sectionTotalMinutes <= nowTotalMinutes {
+                                previousSection = section
+                            } else {
+                                break
+                            }
+                        }
+                        
+                        if let previousSection = previousSection {
+                            DispatchQueue.main.async {
+                                scrollToSectionID = previousSection.date
+                            }
+                        }
+                        
+                        firstAppear = false
+                    }
+                }
+            }
+
+            .navigationBarTitle(L10n.screenAgenda)
+            .navigationBarItems(trailing:
+                                    Menu("\(Image(systemName: "line.3.horizontal.decrease.circle"))") {
+                let selected = Binding(
+                    get: { self.showFavoritesOnly },
+                    set: { self.showFavoritesOnly = $0 == self.showFavoritesOnly ? false : $0 }
+                )
+                Picker("", selection: selected) {
+                    Text(L10n.filterFavorites).tag(true)
+                    Menu(L10n.sessionFiltersDrawerLanguagesLabel) {
+                        let selected = Binding(
+                            get: { self.selectedLanguage },
+                            set: { self.selectedLanguage = $0 == self.selectedLanguage ? nil : $0 })
+                        Picker(L10n.sessionFiltersDrawerLanguagesLabel, selection: selected) {
+                            Text(L10n.languageFrench).tag(Optional(SessionLanguage.french))
+                            Text(L10n.languageEnglish).tag(Optional(SessionLanguage.english))
+                        }
+                    }
+                    Menu(L10n.sessionFiltersDrawerComplexityLabel) {
+                        let selected = Binding(
+                            get: { self.selectedComplexity },
+                            set: { self.selectedComplexity = $0 == self.selectedComplexity ? nil : $0 })
+                        Picker(L10n.sessionFiltersDrawerComplexityLabel, selection: selected) {
+                            Text(L10n.complexityBeginer).tag(Optional(Complexity.beginner))
+                            Text(L10n.complexityIntermediate).tag(Optional(Complexity.intermediate))
+                            Text(L10n.complexityAdvanced).tag(Optional(Complexity.advanced))
+                        }
+                    }
+                    if let rooms = viewModel.roomsContent {
+                        Menu(L10n.sessionFiltersDrawerRoomsLabel) {
+                            let selected = Binding(
+                                get: { self.selectedRoom },
+                                set: { self.selectedRoom = $0 == self.selectedRoom ? nil : $0 })
+                            Picker(L10n.sessionFiltersDrawerRoomsLabel, selection: selected) {
+                                ForEach(rooms, id: \.id) { room in
+                                    Text(room.name).tag(Optional(room))
+                                }
+                            }
+                        }}
+                    Menu(L10n.sessionFiltersDrawerTypeLabel) {
+                        let selected = Binding(
+                            get: { self.selectedSessionType },
+                            set: { self.selectedSessionType = $0 == self.selectedSessionType ? nil : $0 })
+                        Picker(L10n.sessionFiltersDrawerTypeLabel, selection: selected) {
+                            Text(L10n.sessionTypeConference).tag(Optional(SessionType.conference))
+                            Text(L10n.sessionTypeQuickie).tag(Optional(SessionType.quickie))
+                            Text(L10n.sessionTypeCodelab).tag(Optional(SessionType.codelab))
+                        }
+                    }
+                }
+                ///Adds remove button filters
+                if showFavoritesOnly || selectedRoom != nil || selectedLanguage != nil || selectedComplexity != nil || selectedSessionType != nil {
+                    Button(action: {
+                        self.showFavoritesOnly = false
+                        self.selectedRoom = nil
+                        self.selectedLanguage = nil
+                        self.selectedSessionType = nil
+                        self.selectedComplexity = nil
+                    }) {
+                        Label(L10n.filterClear, systemImage: "trash")
+                    }
+                }
+                
+            })
+            .task {
+                RCValues.sharedInstance.fetchCloudValues()
+                await viewModel.observeRooms()
+                await viewModel.observeSessions()
+            }
+        }
+        
+        .onAppear {
+            
+            if scrollToSectionID == nil {
+                scrollToSectionID = viewModel.agendaContent.sections.first?.date
+            }
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            let currentDay = formatter.string(from: Date())
+
+            
+            if currentDay == "2024-10-17" {
+                day = "2024-10-17"
+            } else if currentDay == "2024-10-18" {
+                day = "2024-10-18"
+            } else {
+                day = "2024-10-17"
+            }
+            
+            
+            FirebaseAnalyticsService.shared.pageEvent(page: AnalyticsPage.agenda, className: "AgendaView")
+        }
+    }
+    
+    private func getFilteredSessions(sessions: [AgendaContent.Session]) -> [AgendaContent.Session] {
+        var selectedSession: [AgendaContent.Session] = sessions
+        if let unwrappedLanguage = selectedLanguage {
+            selectedSession = selectedSession.filter { $0.language == unwrappedLanguage }
+        }
+        if let unwrappedSessionType = selectedSessionType {
+            selectedSession = selectedSession.filter { $0.sessionType == unwrappedSessionType }
+        }
+        if let unwrappedRoom = selectedRoom {
+            selectedSession = selectedSession.filter { unwrappedRoom.name.contains($0.room) }
+        }
+        if let unwrappedComplexity = selectedComplexity {
+            selectedSession = selectedSession.filter { $0.complexity == unwrappedComplexity }
+        }
+        return selectedSession
+    }
+}
---
+++ iosApp/iosApp/Agenda/AgendaViewModel.swift
@@ -0,0 +1,146 @@
+//
+//  AgendaViewModel.swift
+//  DevFest Nantes
+//
+//  Created by Stéphane Rihet on 13/09/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+import Combine
+import KMPNativeCoroutinesAsync
+import SwiftUI
+import os
+
+
+class AgendaViewModel: BaseViewModel {
+    @Published var agendaContent: AgendaContent = AgendaContent(sections: [])
+    @Published var roomsContent: [Room_]?
+    @Published var isLoading = true
+    let defaults = UserDefaults.standard
+    
+    private let performanceMonitoring = PerformanceMonitoring.shared
+    
+    var favorites: [String] {
+        get {
+            return defaults.object(forKey: "Favorites") as? [String] ?? []
+        }
+        set {
+            defaults.set(newValue, forKey: "Favorites")
+        }
+    }
+    
+    ///Initialization of the model with store and the UserDefaults object
+    override init() {
+        super.init()
+        self.favorites = defaults.object(forKey: "Favorites") as? [String] ?? []
+    }
+    
+    // MARK: - Favorites management
+
+    ///Method to add or remove a session from favorites
+    func toggleFavorite(ofSession session: AgendaContent.Session) {
+        if favorites.contains(session.id) {
+            removeSessionFromFavorites(sessionId: session.id)
+        } else {
+            addSessionToFavorites(sessionId: session.id)
+        }
+    }
+    
+    ///Method allowing the deletion of the session in favorites
+    private func removeSessionFromFavorites(sessionId: String) {
+        objectWillChange.send()
+        favorites = favorites.filter { $0 != sessionId }
+    }
+
+    private func addSessionToFavorites(sessionId: String) {
+        objectWillChange.send()
+        favorites.append(sessionId)
+    }
+    
+
+    
+    ///Asynchronous method to retrieve sessions
+    func observeSessions() async {
+        do {
+            let sessions = try await performanceMonitoring.trackDataLoad(
+                traceName: PerformanceMonitoring.TRACE_AGENDA_LOAD,
+                dataSource: "graphql"
+            ) {
+                let sessionsSequence = asyncSequence(for: self.store.sessions)
+                var sessions: [Session_] = []
+                
+                for try await sessionsList in sessionsSequence {
+                    sessions = sessionsList
+                    break // Get the first emission
+                }
+                
+                return sessions
+            }
+            
+            DispatchQueue.main.async {
+                self.sessionsChanged(sessions: sessions)
+            }
+            
+            Logger(subsystem: Bundle.main.bundleIdentifier ?? "DevFestNantes", category: "Agenda")
+                .info("Agenda sessions loaded: \(sessions.count) sessions")
+                
+        } catch {
+            Logger(subsystem: Bundle.main.bundleIdentifier ?? "DevFestNantes", category: "Agenda")
+                .error("Observe Sessions error: \(error.localizedDescription)")
+            
+            DispatchQueue.main.async {
+                self.isLoading = false
+            }
+        }
+    }
+    
+    ///Allows you to classify sessions by time section
+    private func sessionsChanged(sessions: [Session_]) {
+        let groupedSessions = Dictionary(grouping: sessions) { getDate(date: $0.scheduleSlot.startDate) }
+        let sortedKeys = groupedSessions.keys.sorted()
+        var sections = [AgendaContent.Section]()
+        sortedKeys.forEach { date in
+            let sessions = groupedSessions[date]!
+                .map { AgendaContent.Session(from: $0) }
+                .sorted { $0.room < $1.room }
+            sections.append(AgendaContent.Section(
+                date: date,
+                day: self.sectionDateFormatter.string(from: date),
+                sessions: sessions))
+        }
+        self.isLoading = false
+        agendaContent.sections = sections
+    }
+    
+    ///Asynchronous method to retrieve rooms
+    func observeRooms() async {
+        Task {
+            do {
+                let roomsSequence = asyncSequence(for: store.rooms)
+                for try await rooms in roomsSequence {
+                    DispatchQueue.main.async {
+                        self.roomsContent = Array(rooms)
+                    }
+                }
+            } catch {
+                Logger(subsystem: Bundle.main.bundleIdentifier ?? "DevFestNantes", category: "Agenda").error("Observe Rooms error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    ///Method to convert a date in string format to ISO 8601 format
+    func getDate(date: String) -> Date {
+        let newFormatter = ISO8601DateFormatter()
+        return newFormatter.date(from: date) ?? Date() // replace Date String
+    }
+    
+    ///Formatting the date
+    var sectionDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-d"
+        return formatter
+    }()
+}
+
