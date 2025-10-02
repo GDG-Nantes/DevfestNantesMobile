b62215537340d59fdd1ba9cb07043a4daf1eb0d4
Robin Caroff <robin.caroff@gmail.com>
2025-10-02 22:08:00 +0200

    fix: increment versionCode to 37 and update versionName to 2025.10.00

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.gdgnantes.devfest.mobile.androidapp"
         minSdk = AndroidSdk.min
         targetSdk = AndroidSdk.target
-        versionCode = 36
-        versionName = "2025.09.02"
+        versionCode = 37
+        versionName = "2025.10.00"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "OPEN_FEEDBACK_ENABLED", "\"false\"")
